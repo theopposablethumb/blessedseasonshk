0d6c23aa1c624882aad3da88215bd9e8bc76ecc2
Brendan Meachen <brendan.meachen@gmail.com>
2023-08-17 14:54:33 +0100

    init landing page

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const copyrightYear = () => {
+    document.querySelector('footer span').innerHTML = new Date().getFullYear();
+  }
+  
+  
+  const makeItSo = () => {
+    document.addEventListener("DOMContentLoaded", () => {
+      copyrightYear();
+      console.log("This website crafted by hand by Brendan Meachen");
+   });
+  };
+  makeItSo();
